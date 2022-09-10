df63bb9b53bcd9b3086994e4e3feb8c78ef619f1
Ahmed Taha <ahmed.taha8399@gmail.com>
2022-09-10 17:00:45 +0200

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Ahmed Taha on 10/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
